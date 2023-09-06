9b225d75fb9eae7eda9d6840c51dcee82bb04722
blandero <landerosb08@gmail.com>
2023-09-05 22:20:25 -0600

    Adding js file and updating html

---
+++ Vault.js
@@ -0,0 +1,17 @@
+/* Pseudo code for Securing the Vault:,
+1. The first step will be to create an HTML and JavaScript files and link them together.
+2. Declare the variables for each of the combo numbers.
+3.
+4.
+5.
+6.
+7.
+*/
+const greeting ="You have received this message because you have been chosen to open an important vault. Here is the secret combination:" 
+        let numCombo1 = 10;
+        let numCombo2 = 40;
+        let numCombo3 = 39;
+        const numCombo1 = 10 / 5;
+        const numCombo2 = 5 * 8;
+        const numCombo3 = 28 + 11;
+        alert (Secret Combo: 10- 40- 39);
